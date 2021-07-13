4cb6792894ec7b64766c25545420f69ee5347b43
hazvick <44568059+hazvick@users.noreply.github.com>
2021-07-13 12:50:42 +0200

    Create README.ml

---
+++ SQL-Movies/README.ml
@@ -0,0 +1,8 @@
+SQL Movies Assignment
+
+queries that {
+  - lists all actors for the movie Seven
+  - lists who's gotten an oscar
+  - lists movies Brad Pitt has been in
+  - lists the awards Brad Pitt has gotten
+}
