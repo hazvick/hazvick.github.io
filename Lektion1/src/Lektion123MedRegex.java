8705b7527c6c6bb9eeb28c8814d8140f7319b456
hazvick <44568059+hazvick@users.noreply.github.com>
2021-09-07 11:36:59 +0200

    RegexPassword

---
+++ Lektion1/src/Lektion123MedRegex.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class PasswordRegex {
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	System.out.println(
+    			"Thank you for joining r/dankmemes, please create a password.\n" +
+    			"- length between 6-12 \n" + 
+    			"- has to contain at least 2 numbers \n" + 
+    			"Please enter your new password: " 
+    			);
+    	while (true) { 
+        	String password = scan.nextLine();
+    		if (password.matches("(?!.*[^A-Za-z0-9])(?=.{6,12}).*\\d.*\\d.*")) { //implementering av regex
+        	    System.out.println("Password is valid");
+        	    scan.close(); //stänga scanner för att förhindra resource leak
+        	    break;
+        	} else {
+        		System.err.println("The password: " + password + " is invalid. Try again.");
+        	}
+    	}
+    }
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
